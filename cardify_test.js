699944abb91dd2c7843c818a83df214df909f450
ButrikDenis <38683503+skylinedb@users.noreply.github.com>
2025-01-29 22:58:27 +0300

    Update cardify_test.js

--- cardify_test.js
+++ cardify_test.js
@@ -998,8 +998,6 @@
   };
 
   function startPlugin() {
-    if (!Lampa.Platform.screen('tv')) return console.log('Cardify', 'no tv');
-    if (!Lampa.Account.hasPremium()) return console.log('Cardify', 'no premium');
     Lampa.Lang.add({
       cardify_enable_sound: {
         ru: 'Включить звук',
